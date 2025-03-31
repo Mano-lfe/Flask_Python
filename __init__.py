b30fb033c97f46a0ff148ecbbc99f7d39ac224db
Mano-lfe <manolife06@gmail.com>
2025-03-31 14:20:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ def exercice(valeur):
         etoiles = ''
     for j in range(valeur):
         for i in range(valeur):
-            etoiles += '*'
+            etoiles += '*' * i+1
         etoiles += '<br>'
     return etoiles
 
