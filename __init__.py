5f370313026ea06552683fa10b0f9163755cc6b6
Mano-lfe <manolife06@gmail.com>
2025-04-07 10:28:24 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,17 +4,22 @@ from flask import json
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
-def exercice(valeur):
-   nombres = ''   
+   resultat = ''
     for i in range(1, valeur + 1):
+        # Espaces pour centrer
+        resultat += '&nbsp;' * (valeur - i)
         
-      nombres += '' * (valeur - i)
+      
         for j in range(1, i + 1):
-           nombres += (j)
+            resultat += str(j)
+        
+     
         for j in range(i - 1, 0, -1):
-            nombres += str(j)
-        nombres += '<br>'
-    return nombres
+            resultat += str(j)
+
+        resultat += '<br>'
+    
+    return resultat
 
 if __name__ == "__main__":
     app.run(debug=True)
