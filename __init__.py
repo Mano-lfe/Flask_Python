e7705d1d6fb38cb5b8913b69951002f33522364f
Mano-lfe <manolife06@gmail.com>
2025-04-07 15:34:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def exercice(valeurs):
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
-    max_valeur = liste_nombres[0]
-    for n in liste_nombres:
-        if n > max_valeur:
-            max_valeur = n
-    
-    return str(max_valeur)
+
+    for i in range(len(liste_nombres) - 1):
+        if liste_nombres[i] < liste_nombres[i + 1]:
+            liste_nombres[i + 1] = liste_nombres[i]
+
+    return f"Le nombre minimum est : {liste_nombres[-1]}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
