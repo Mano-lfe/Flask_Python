07a552ffa7665e07f05e6add0653cc6408635a44
Mano-lfe <manolife06@gmail.com>
2025-04-07 12:28:53 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,30 +1,22 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from flask import json
 
-@app.route('/<int:n>')
-def exercice(n):
+
+app = Flask(name)
+
+@app.route('/<int:valeur>')
+def exercice(valeur):
     somme = 0
-    resultat = f"<h2>Exercice 3 : NOUVEAU</h2>"
-    resultat += f"<p>Calcul de la somme des nombres de 1 à {n} selon les conditions suivantes :</p>"
-    resultat += "<ul>"
-    resultat += "<li>Ajouter à la somme les nombres divisibles par 5 ou 7</li>"
-    resultat += "<li>Ignorer les nombres divisibles par 11</li>"
-    resultat += "<li>Arrêter si la somme dépasse 5000</li>"
-    resultat += "</ul><p>Détails :<br>"
-
-    for i in range(1, n + 1):
+    for i in range(1, valeur + 1):
         if i % 11 == 0:
-            resultat += f"{i} ignoré (divisible par 11)<br>"
             continue
         if i % 5 == 0 or i % 7 == 0:
+            if somme + i > 5000:
+                break
             somme += i
-            resultat += f"{i} ajouté, somme actuelle = {somme}<br>"
-        if somme > 5000:
-            resultat += "⛔ Somme dépasse 5000, arrêt du programme.<br>"
-            break
 
-    resultat += f"</p><p><strong>Somme finale :</strong> {somme}</p>"
-    return resultat
+    return f'<pre>Somme finale: {somme}</pre>'
 
-if __name__ == "__main__":
+if name == "main":
     app.run(debug=True)
