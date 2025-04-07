7d09ea0a9857ff847a8e7efeeddbd96e5731af71
Mano-lfe <manolife06@gmail.com>
2025-04-07 10:26:38 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
+from flask import json                                                                                                                                     
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-   nombres = ''
-    
+   nombres = ''   
     for i in range(1, valeur + 1):
         
       nombres += '' * (valeur - i)
