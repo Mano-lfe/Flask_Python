9f2bc7d2eb4cd2b8bd814aad762aa52236bf93e3
Mano-lfe <manolife06@gmail.com>
2025-04-07 10:33:16 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from flask import render_template
-from flask import json                                                                                                                                     
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
-   resultat = ''
+def exercice(valeur):
+    resultat = ''
     for i in range(1, valeur + 1):
-    
+        # Espaces pour centrer
         resultat += '&nbsp;' * (valeur - i)
         
-      
+        # Nombres croissants
         for j in range(1, i + 1):
             resultat += str(j)
         
-     
+        # Nombres décroissants
         for j in range(i - 1, 0, -1):
             resultat += str(j)
 
+        # Saut de ligne HTML
         resultat += '<br>'
     
     return resultat
