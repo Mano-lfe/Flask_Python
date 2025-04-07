8a36c8acebc5fc2c13976cc2b929dfc56858db1b
Mano-lfe <manolife06@gmail.com>
2025-04-07 10:52:16 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,9 +11,6 @@ def exercice(valeur):
         a, b = b, a + b       # Mise à jour des deux derniers termes
 
     # On transforme la liste en chaîne de caractères séparée par des virgules
-    resultat = f"<h2>Exercice 2 : Suite de calculs</h2>"
-    resultat += f"<p>On part du postulat que a = 0 et b = 1</p>"
-    resultat += f"<p>Résultat : {', '.join(suite)}</p>"
 
     return resultat
 
