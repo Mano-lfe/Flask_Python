e37eb4e7686ceee7a2fa8c47ebe5bef36e449c1a
Mano-lfe <manolife06@gmail.com>
2025-04-07 10:50:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,22 +3,18 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    resultat = ''
-    for i in range(1, valeur + 1):
-        # Espaces pour centrer
-        resultat += '&nbsp;' * (valeur - i)
-        
-        # Nombres croissants
-        for j in range(1, i + 1):
-            resultat += str(j)
-        
-        # Nombres décroissants
-        for j in range(i - 1, 0, -1):
-            resultat += str(j)
+    a, b = 0, 1  # Les deux premiers termes
+    suite = []
 
-        # Saut de ligne HTML
-        resultat += '<br>'
-    
+    for i in range(valeur):
+        suite.append(str(a))  # On ajoute le terme actuel à la liste
+        a, b = b, a + b       # Mise à jour des deux derniers termes
+
+    # On transforme la liste en chaîne de caractères séparée par des virgules
+    resultat = f"<h2>Exercice 2 : Suite de calculs</h2>"
+    resultat += f"<p>On part du postulat que a = 0 et b = 1</p>"
+    resultat += f"<p>Résultat : {', '.join(suite)}</p>"
+
     return resultat
 
 if __name__ == "__main__":
